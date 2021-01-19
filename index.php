ecc30af22a96686ebfa67a7eff33930ad614a25c
hneschen <hanes_mans@yahoo.com>
2021-01-19 21:23:46 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum deserunt libero minima! Sed totam incidunt mollitia odit aliquid saepe impedit deleniti molestias repellendus molestiae, corrupti ducimus nihil libero, consequuntur amet.</p>
+</body>
+</html>
